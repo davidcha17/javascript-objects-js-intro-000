359c1833f09477b84a2e7b66c74fe523063baa30
David Cha <david.cha44@qmail.cuny.edu>
2019-10-22 16:33:20 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,1 +1,7 @@
 var playlist = {'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"}
+
+
+function updatePlayList(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle
+  return playlist
+}
