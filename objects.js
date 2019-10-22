0f2e66fdc7934d1ccaa9cb02b69fbffba59f985c
David Cha <david.cha44@qmail.cuny.edu>
2019-10-22 16:52:03 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,18 +2,18 @@ var playlist = {
   'Alison': 'My Bloody Valentine', 'Phil Ochs': 'Here\'s to The State of Mississippi'
 }
 
-
+/*
 function updatePlayList(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
   return playlist
 }
+*/
 
-/*
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
   return playlist
 }
-*/
+
 function removeFromPlaylist(playlist, artistName, songTitle){
   delete playlist[artistName]
   return playlist
