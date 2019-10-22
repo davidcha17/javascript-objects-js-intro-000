60280596f92aee889845be89aa4ae9a3bdd506f5
David Cha <david.cha44@qmail.cuny.edu>
2019-10-22 16:40:35 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,3 +7,8 @@ function updatePlayList(playlist, artistName, songTitle){
   playlist[artistName] = songTitle
   return playlist
 }
+
+function removeFromPlaylist(playlist, artistName, songTitle){
+  delete playlist(artistName)
+  return playlist 
+}
